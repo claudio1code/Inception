0bd92597d60b9a9ccd518fbbd8ebafeea3c1dfba
Claudio <claudio1.code@gmail.com>
2026-06-04 17:54:35 -0300

    fix(scripts): fix paths nginx and mariadb

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Cria o diretório para os certificados se não existir
+# Create necessary directories for nginx
 mkdir -p /etc/nginx/ssl
-
-# Gera o certificado autoassinado usando TLSv1.3 se ele não existir
+ 
+# Generate silence self-signed SSL certificate
 if [ ! -f /etc/nginx/ssl/inception.crt ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
-        -keyout /etc/nginx/ssl/inception.key \
-        -out /etc/nginx/ssl/inception.crt \
-        -subj "/C=BR/ST=SP/L=SaoPaulo/O=42SP/OU=Cadet/CN=clados-s.42.fr"
+	openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
+		-keyout /etc/nginx/ssl/inception.key \
+		-out /etc/nginx/ssl/inception.crt \
+		-subj "/C=BR/ST=SP/L=SaoPaulo/O=42SP/OU=Cadet/CN=clados-s.42.fr"
+		> /dev/null 2>&1
 fi
 
-# Executa o comando principal do NGINX passado pelo Dockerfile (foreground)
+echo "NGINX is configured and starting..."
+
+# Start nginx by replacing the script's process (PID 1)
 exec "$@"
